8a63992909327814a48de9be9ce62b7eaca04be4
DDiegoR <91162803+DDiegoR@users.noreply.github.com>
2022-11-18 19:01:36 -0300

    correo v.2

--- enviar.php
+++ enviar.php
@@ -1,22 +1,22 @@
 <?php
 $name = $_POST['name'];
-$name = $_POST['mail'];
-$name = $_POST['message'];
-
-$header = 'From: ' . $mail . " \r\n";
-$header = 'X-Mailer: PHP/' . phpversion() . " \r\n";
-$header = "Mime-Version: 1.0 \r\n";
-$header = "Content-Type: text/plain \r\n";
-
-$message = "Este mensaje fue enviado por: ".$name " \r\n";
-$message = "Su correo electrónico es: ".$mail " \r\n";
-$message = "Mensaje: " . $_POST['message'] . " \r\n";
-$message = "Enviado el: " . date('d/m/Y', time());
+$mail = $_POST['mail'];
+$message = $_POST['message'];
 
 $para = 'diegoromerofrre@gmail.com';
 $asunto = 'Mensaje del Portfolio';
 
-mail($para, &asunto, utf8_decode($message), $header);
+$header = 'From: ' . $mail . " \r\n";
+$header .= 'X-Mailer: PHP/' . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain \r\n";
 
-header("Location:index.html");
+$message = "Este mensaje fue enviado por: "$name " \r\n";
+$message .= "Su correo electrónico es: "$mail " \r\n";
+$message .= "Mensaje: "$message " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
+
+mail($para, $asunto, $message);
+
+header('Location:index.html');
 ?>
