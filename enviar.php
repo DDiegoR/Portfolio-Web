79fcdc0f87dbe0fea13ad0f7a86c1f3e8101f88f
DDiegoR <91162803+DDiegoR@users.noreply.github.com>
2022-11-18 20:18:30 -0300

    correo v.4

--- enviar.php
+++ enviar.php
@@ -6,16 +6,16 @@ $message = $_POST['message'];
 $para = 'diegoromerofrre@gmail.com';
 $asunto = 'Mensaje del Portfolio';
 
-$header = "From: $nombre $mail \r\n";
-$header .= "X-Mailer: PHP5\n";
-$header .= 'MIME-Version: 1.0' . "\n";
-$header .= 'Content-Type: text/html; charset=iso-8859-1' . "\r\n";
+$header = 'From: ' . $mail  . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "MIME-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
 
 $message = "Este mensaje fue enviado por: ".$name." <br>";
 $message .= "Su correo electrónico es: ".$mail."<br>";
-$message .= "Mensaje: ".$message;
+$message .= "Mensaje: " . $_POST['message'] . " \r\n";
 
-mail($para, $asunto, $message, $header);
+mail($para, $asunto, utf8_decode($message), $header);
 
 header('Location:index.html');
 ?>
